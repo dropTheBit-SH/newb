37dbabdb38bfb7f9803a6d05413462ff417fd302
Suhee Kim <62128361+dropTheBit-SH@users.noreply.github.com>
2020-04-05 20:17:24 +0900

    Update OutputAll.java

--- src/scoremng/view/OutputAll.java
+++ src/scoremng/view/OutputAll.java
@@ -13,9 +13,16 @@ public class OutputAll implements View {
 		}
 		System.out.println();
 
-		for(int i=0; i<data.dtoArr.length; i++){
-			System.out.printf(data.dtoArr[i].toString()+(i+1)+"\n");
+		System.out.printf(data.dtoArr[0].toString()+1+"\n");
+		for(int i=1; i<data.dtoArr.length; i++){
+			if(!(data.dtoArr[i].getAvg() == data.dtoArr[i-1].getAvg())) {
+				System.out.printf(data.dtoArr[i].toString()+(i+1)+"\n");
+			}else {
+				System.out.printf(data.dtoArr[i].toString()+(i)+"\n");
+			}
+			  
 		}
+		
 		System.out.println(endTitle);
 	}
 

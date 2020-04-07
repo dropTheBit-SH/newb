50db466537f14e5e59a7bb8d80d6636e82d10a3e
Suhee Kim <62128361+dropTheBit-SH@users.noreply.github.com>
2020-04-08 00:38:20 +0900

    Create c7_8.java

---
+++ src/HW/book/chapter7/c7_8.java
@@ -0,0 +1,35 @@
+package chapter7;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class c7_8 {
+	static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		HashMap<String, Integer> pointMgmt = new HashMap<String, Integer>();
+		System.out.println("** 포인트 관리 프로그램입니다 **");
+		
+		while(true) {
+			System.out.print("이름과 포인트 입력>> ");
+			String name = scan.next();
+			if(name.equals("그만")) break;
+			int point = scan.nextInt();
+			if(pointMgmt.get(name)==null)	pointMgmt.put(name, point);
+			else pointMgmt.put(name, point+pointMgmt.get(name));
+			
+			Set<String> key = pointMgmt.keySet();
+			Iterator<String> it = key.iterator();
+			while(it.hasNext()) {
+				String who = it.next();
+				Integer sum = pointMgmt.get(who);
+				System.out.printf("(%s,%d)",who,sum);
+			}
+			System.out.println();
+			
+			
+		}
+	}
+}
